c1820031e271f6a2c56ebd6d1216b8725c35369c
Winthuraaung <Ronaldwu5@gmail.com>
2024-06-14 19:01:23 -0700

    first

---
+++ TrainTokenizer.py
@@ -0,0 +1,38 @@
+import sys
+from datasets import load_dataset
+
+sys.path.append('/Users/macbookpro/Desktop/BaseInternship')
+
+from Tokenizer.Basic_Tokenizer import basic_tokenizer
+class TrainTokenizer:
+    def __init__(self, vocab_size):
+        self.dataset = load_dataset("json", data_files="/Users/macbookpro/Desktop/BaseInternship/oscar-2019-my-fix.json", split="train")
+        self.trainer = basic_tokenizer()
+        self.vocab_size = vocab_size
+        self.text = ""
+
+    def concatenateString(self):
+        count = 1
+        for doc in self.dataset:
+            print(count)
+            count += 1
+            text = doc['text']
+            self.text += text + "\n"
+
+    def getText(self):
+        return self.text
+
+    def train(self):
+        self.trainer.train(self.text, 300)
+    
+    def num_of_tokens(self, word):
+        return len(self.encode(word))
+    
+    def encode(self, word):
+        return self.trainer.encode(word)
+
+TrainTokenizer = TrainTokenizer(300)
+TrainTokenizer.concatenateString()
+test_phrase = "မင်္ဂလာပါ"
+print(f"Encoded phrase: {TrainTokenizer.encode(test_phrase)}")
+print(f"Number of tokens: {TrainTokenizer.num_of_tokens(test_phrase)}")
